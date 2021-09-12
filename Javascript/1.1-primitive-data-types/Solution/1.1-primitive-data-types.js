7ebddda1698b344644f8817277938a5703c354a7
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-09-12 10:43:22 +0300

    added JS exercise 1.1

---
+++ Javascript/1.1-primitive-data-types/Solution/1.1-primitive-data-types.js
@@ -0,0 +1,15 @@
+let str = 'string for 1';
+let num = '1';
+let obj = str;
+let isTrue = false;
+let nullVar = null;
+let undefVar = undefined;
+let symbolVar = Symbol('some symbol');
+
+console.log('str =',str);
+console.log('num =',num);
+console.log('obj =',typeof(obj));
+console.log('isTrue =',isTrue);
+console.log('nullVar =',nullVar);
+console.log('undefVar =',undefVar);
+console.log('symbolVar =',symbolVar);
