8560c9bbbe155e0e44f9c7d96bb009401abedacb
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-09-14 11:20:56 +0300

    added exercise 7.1

---
+++ Javascript/7.1-book/Solution/7.1-book.js
@@ -0,0 +1,14 @@
+Book = {
+    Title: "Death Note",
+    Writer: "Nisio Isin",
+    ReleaseDate: "2006",
+    Revision: 1,
+    isHardCover: false
+}
+
+function bookDescription(book) {
+    return (`The book ${book.Title} was written by ${book.Writer} in the year ${book.ReleaseDate}`);
+}
+
+console.log(Book);
+console.log(bookDescription(Book));
