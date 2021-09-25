54cd621084f0ff80d39038180d5e32cc4e10eb86
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-09-25 23:00:28 +0300

    fixed some small things

--- Project - Weekend Javascript/EX7-Filter-ForEach-Map.js
+++ Project - Weekend Javascript/EX7-Filter-ForEach-Map.js
@@ -24,12 +24,19 @@ Array.prototype.myMap = function(callback){
 let arr = [0,1,2,3,4,-5,-6,-7,-8,9,10];
 let resArr1 = arr.filter(x => x > 0); //check if positive
 let resArr2 = arr.myFilter(x => x > 0); //check if positive
+console.log('Javascript filter()');
 console.log(resArr1);
+console.log('My Filter()');
 console.log(resArr2);
 
 console.log();
 
 let arr2 = [1,2,-1,-2];
+console.log('Javascript forEach()');
+arr2.forEach(element => {
+    console.log(element);
+})
+console.log('My forEach()');
 arr2.myForEach(element => {
     console.log(element);
 })
@@ -40,4 +47,10 @@ let arr3 = [1,2,3,4];
 let resArr3 = arr3.myMap(element => {
     return element*element;
 })
+let resArr4 = arr3.map(element => {
+    return element*element;
+})
+console.log('Javascript Map()');
+console.log(resArr4);
+console.log('My Map()');
 console.log(resArr3);
