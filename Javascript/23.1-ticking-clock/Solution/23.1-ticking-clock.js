dcaee2ee188d66dabedebacf2a72a6375ba87886
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-09-28 16:10:41 +0300

    added 23.1

---
+++ Javascript/23.1-ticking-clock/Solution/23.1-ticking-clock.js
@@ -0,0 +1,22 @@
+const hours = document.querySelector('.hours')
+const minutes = document.querySelector('.minutes')
+const seconds = document.querySelector('.seconds')
+setInterval(() => {
+    let today = new Date();
+    let calchours = today.getHours();
+    let calcminutes = today.getMinutes();
+    let calcseconds = today.getSeconds();
+
+    if (calchours < 10)
+        hours.innerText = `0${calchours}`;
+    else
+        hours.innerText = `${calchours}`;
+    if (calcminutes < 10)
+        minutes.innerText = `0${calcminutes}`;
+    else
+        minutes.innerText = `${calcminutes}`;
+    if (calcseconds < 10)
+        seconds.innerText = `0${calcseconds}`;
+    else
+        seconds.innerText = `${calcseconds}`;
+}, 500);
