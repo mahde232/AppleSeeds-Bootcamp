8b0e50e040a012c14e932ae7699f584622eb51ca
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-09-28 09:41:36 +0300

    minor fixes

--- Javascript/17.2-text-area/Solution/17.2-text-area.js
+++ Javascript/17.2-text-area/Solution/17.2-text-area.js
@@ -1,13 +1,26 @@
 const textArea = document.querySelector('#textArea');
 const btn = document.querySelector('#btn');
+const errorText = document.querySelector('#errorText')
 
+// btn.addEventListener('click',function() {
+//     console.log(textArea.value.length);
+//     if(textArea.value.length < 100)
+//         {btn.style.transition = 'all 1s'
+//         btn.style.background = 'red';
+//         setInterval(() => {
+//             btn.style.background = 'none';
+//         },1100)
+//     }
+// })
 btn.addEventListener('click',function() {
     console.log(textArea.value.length);
     if(textArea.value.length < 100)
-        {btn.style.transition = 'all 1s'
-        btn.style.background = 'red';
-        setInterval(() => {
-            btn.style.background = 'none';
-        },1100)
+    {
+        errorText.innerHTML = 'Amount of characters is below 100 <a href="#">asdasd.com</a>';
+        errorText.style.color = 'red'
+    }
+    if(errorText.style.visibility = "visible" && textArea.value.length >= 100)
+    {
+        errorText.style.visibility = "hidden"
     }
 })
