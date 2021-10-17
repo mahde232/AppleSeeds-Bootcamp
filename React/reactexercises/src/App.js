6770ed64a4a27e89da9cb0c96c4db5c175065e63
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-10-17 16:56:08 +0300

    added exercise 4.2

--- React/reactexercises/src/App.js
+++ React/reactexercises/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 // import Boxes from './components/3.1-boxes/3.1-boxes';
 // import Quiz from './components/3.2-quiz/3.2-quiz';
 // import Button from './components/4.1-buttons/Button';
+import Card from './components/4.2-cards/Card';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
       {/* <Quiz/> */}
       {/* <Button name='Important' weight='900'/>
       <Button name='Not Important' weight='100'/> */}
+      <Card imgURL='https://picsum.photos/200' title='Picture 1' description='Random Description for picture 1'/>
+      <Card imgURL='https://picsum.photos/200' title='Picture 2' description='Random Description for picture 2'/>
+      <Card imgURL='https://picsum.photos/200' title='Picture 3' description='Random Description for picture 3'/>
     </div>
   );
 }
---
+++ React/reactexercises/src/components/4.2-cards/Card.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+const Card = ({imgURL, title, description}) => {
+    return <div className='card'>
+        <img src={imgURL}></img>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <div><a href='#' className='link'>Share</a> <a href='#' className='link'>Explore</a></div>
+    </div>
+}
+
+export default Card;
