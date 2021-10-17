6f1f890245d68b30e216e8c9fda7377abecab54c
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-10-17 12:47:29 +0300

    fixed wrong naming on boxes exercise

--- React/reactexercises/src/App.js
+++ React/reactexercises/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import HelloWorld from './components/2.1-hello-world/2.1.helloWorld';
-import Basics from './components/2.2-basics-of-jsx/2.2-basics-of-jsx';
-import Boxes from './components/3.1-boxes/3.2-boxes';
+// import HelloWorld from './components/2.1-hello-world/2.1.helloWorld';
+// import Basics from './components/2.2-basics-of-jsx/2.2-basics-of-jsx';
+// import Boxes from './components/3.1-boxes/3.1-boxes';
 import Quiz from './components/3.2-quiz/3.2-quiz';
 
 function App() {
