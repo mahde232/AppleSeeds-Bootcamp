6c64936712d215d2862c1c7308885529c3e0c8fe
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-10-21 14:46:12 +0300

    Update Spinner.js

--- React/reactexercises/src/components/9.1-spinner/Spinner.js
+++ React/reactexercises/src/components/9.1-spinner/Spinner.js
@@ -13,12 +13,14 @@ class Spinner extends React.Component {
             this.setState({
                 isVisible: !this.state.isVisible
             })
-        }, 2000);
+        }, 3000);
     }
     render() {
-        const spinners = [<div class="loader" style={{display: this.state.isVisible ? 'block' : 'none'}}>Loading...</div>,
+        const spinners = [
+            <div class="loader" style={{display: this.state.isVisible ? 'block' : 'none'}}>Loading...</div>,
             <div class="loader2" style={{display: this.state.isVisible ? 'block' : 'none'}}>Loading...</div>,
-            <div class="loader3" style={{display: this.state.isVisible ? 'block' : 'none'}}>Loading...</div>]
+            <div class="loader3" style={{display: this.state.isVisible ? 'block' : 'none'}}>Loading...</div>
+        ]
         return spinners[Math.floor(Math.random() * 3)];
     }
 }
