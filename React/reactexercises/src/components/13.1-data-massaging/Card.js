1dabed35481828da0d48fd3f51704333409a7cb9
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-10-24 18:55:02 +0300

    removed useless constructors

--- React/reactexercises/src/components/13.1-data-massaging/Card.js
+++ React/reactexercises/src/components/13.1-data-massaging/Card.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
 import Name from './Name'
 
 export default class Card extends Component {
-    constructor(props){
-        super(props)
-    }
     render() {
         return (
             <Name name={this.props.object.name}/>
--- React/reactexercises/src/components/13.1-data-massaging/Name.js
+++ React/reactexercises/src/components/13.1-data-massaging/Name.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 
 export default class Name extends Component {
-    constructor(props){
-        super(props);
-    }
     render() {
         return (
             <div>{this.props.name}</div>
