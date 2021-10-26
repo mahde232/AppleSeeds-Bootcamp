8098d2b700bed375c59d93aea02047c2550d6d9c
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-10-26 09:25:55 +0300

    added 2 methods to solve the question

--- React/reactexercises/src/components/19.3-algolia/Alogolia.js
+++ React/reactexercises/src/components/19.3-algolia/Alogolia.js
@@ -1,25 +1,38 @@
 import React from "react";
 
 export default function Alogolia() {
-    const [hooks, setHooks] = React.useState([]);
+    const [results, setResults] = React.useState([]);
     const [searchTerm, setSearchterm] = React.useState("hooks");
-    const [searchValue, setSearchValue] = React.useState("hooks");
+    // const [searchValue, setSearchValue] = React.useState("hooks"); //use this to us the "tricking-method" of setting searchValue according to searchTerm using onClick.
 
     React.useEffect(() => {
-        const myFunc = async () => {
-            let result = await (
-                await fetch(`https://hn.algolia.com/api/v1/search?query=${searchValue}`)
-            ).json();
-            console.log(result.hits);
+        // const myFunc = async () => {
+        //     let result = await (
+        //         await fetch(`https://hn.algolia.com/api/v1/search?query=${searchValue}`)
+        //     ).json();
+        //     console.log(result.hits);
 
-            let searchedData = result.hits.map((ele) => {
-                return { title: ele.title, url: ele.url };
-            });
-            console.log(searchedData);
-            setHooks(searchedData);
-        };
+        //     let searchedData = result.hits.map((ele) => {
+        //         return { title: ele.title, url: ele.url };
+        //     });
+        //     console.log(searchedData);
+        //     setHooks(searchedData);
+        // };
         myFunc();
-    }, [searchValue]);
+    // }, [searchValue]);
+    }, []);
+
+    
+    const myFunc = async () => {
+        let result = await (
+            await fetch(`https://hn.algolia.com/api/v1/search?query=${searchTerm}`)
+        ).json();
+
+        let searchedData = result.hits.map((ele) => {
+            return { title: ele.title, url: ele.url };
+        });
+        setResults(searchedData);
+    };
 
     return (
         <div>
@@ -33,10 +46,11 @@ export default function Alogolia() {
                 type="button"
                 value="Search"
                 onClick={() => {
-                    setSearchValue(searchTerm);
+                    // setSearchValue(searchTerm);
+                    myFunc()
                 }}
             />
-            {hooks.map((e, index) => {
+            {results.map((e, index) => {
                 return (
                     <li key={index}>
                         <a href={e.url}>{e.title}</a>
